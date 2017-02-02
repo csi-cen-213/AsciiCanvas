b4f96895ee53517a27ce3dca778228f76de470c8
cmagnus <cmagnus@gmail.com>
2017-02-02 15:52:39 -0500

    Adds constructor and fillCanvas method for use in constructor

--- AsciiCanvas.java
+++ AsciiCanvas.java
@@ -1,11 +1,23 @@
 public class AsciiCanvas{
   // Properties
-  int sizeX;
-  int sizeY;
+  int sizeX = 10;
+  int sizeY = 20;
   char[][] canvas = new char[sizeX][sizeY];
-  int drawLocationX;
-  int drawLocationY;
+  int drawLocationX = sizeX/2;
+  int drawLocationY = sizeY/2;
   
-   
+  // Constructors
+  AsciiCanvas() {
+    this.fillCanvas(' ');
+  }
+  
+  // Methods
+  
+  void fillCanvas(char character) {
+    for (int i = 0; i < this.sizeX; i++) 
+      for (int j = 0; j < this.sizeY; j++)
+        this.canvas[i][j] = character;
+  }
+ 
 
 }
--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
 public class Main{
   public static void main(String [ ] args) {
+    AsciiCanvas canvas = AsciiCanvas();
+    System.out.println(canvas.sizeX);
+    System.out.println(canvas.sizeY);
+    System.out.println(canvas.canvas[0][0]);
+    System.out.println(canvas.drawLocationX);
+    System.out.println(canvas.drawLocationY);
   }
 }
