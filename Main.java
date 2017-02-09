66eb7ed3452425a4090569a5b9d0b5d31427d1ea
cmagnus <cmagnus@gmail.com>
2017-02-09 11:27:42 -0500

    Makes constructor testing more explicit. Adds testing for getters/setters

--- Main.java
+++ Main.java
@@ -1,11 +1,25 @@
 public class Main{
   public static void main(String [ ] args) {
-    AsciiCanvas canvas = new AsciiCanvas('*');
-    System.out.println(canvas.getSizeX());
-    System.out.println(canvas.getSizeY());
-    System.out.println(canvas.getDrawLocationX());
-    System.out.println(canvas.getDrawLocationY());
-    canvas.render();
+    // Test constructor
+    AsciiCanvas canvasSpace = new AsciiCanvas();
+    canvasSpace.render();
+    // test getters
+    System.out.println(canvasSpace.getSizeX());
+    System.out.println(canvasSpace.getSizeY());
+    System.out.println(canvasSpace.getDrawLocationX());
+    System.out.println(canvasSpace.getDrawLocationY());
+    // test setters
+    // picking a different number for X and Y so when I 
+    // print them I can tell them apart and make sure they
+    // aren't getting mixed up.
+    canvasSpace.setDrawLocationX(0);
+    canvasSpace.setDrawLocationY(1); 
+    System.out.println(canvasSpace.getDrawLocationX());
+    System.out.println(canvasSpace.getDrawLocationY());
+    // Test constructor that takes char argument
+    AsciiCanvas canvasStar = new AsciiCanvas('*');
+    canvasStar.render();
+    
  
   }
 }
